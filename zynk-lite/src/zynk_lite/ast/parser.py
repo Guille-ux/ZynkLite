4f1bc168f392144effac8d0b8d33f10aef3c9790
Guille on a Raspberry pi <guilleleiratemes@gmail.com>
2025-04-20 02:01:34 +0200

    haciendo parsers

--- zynk-lite/src/zynk_lite/ast/parser.py
+++ zynk-lite/src/zynk_lite/ast/parser.py
@@ -9,11 +9,45 @@ class AlgebraicParser:
     def __init__(self, tokens, debug=False):
         self.tokens = tokens
         self.debug = debug
+        self.current = 0
+    def is_at_end(self):
+        return self.current >= len(tokens)
+    def advance(self):
+        if not self.is_at_end():
+            self.current += 1
+        return self.tokens[self.current]
+    def peek(self):
+        if not self.is_at_end():
+            return self.tokens[self.current+1]
+        return None
+    def eat(self, type):
+        if self.peek().type == type:
+            self.advance()
+            return True
+        return False
+
 
 class ZynkLParser:
     def __init__(self, tokens, debug=False):
         self.debug = debug
         self.tokens = tokens
+        self.current = 0
     def parse(self):
+        pass
         # lógica para parsear
-        # return parsed 
+        # return parsed
+    def is_at_end(self):
+        return self.current >= len(tokens)
+    def advance(self):
+        if not self.is_at_end():
+            self.current += 1
+        return self.tokens[self.current]
+    def peek(self):
+        if not self.is_at_end():
+            return self.tokens[self.current+1]
+        return None
+    def eat(self, type):
+        if self.peek().type == type:
+            self.advance()
+            return True
+        return False
