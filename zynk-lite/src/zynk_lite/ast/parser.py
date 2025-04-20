7c5e7fd501b1684599aa4bd2d27e86d3b8e04485
Guille on a Raspberry pi <guilleleiratemes@gmail.com>
2025-04-20 02:50:35 +0200

    siguiendo con esta cosa

--- zynk-lite/src/zynk_lite/ast/parser.py
+++ zynk-lite/src/zynk_lite/ast/parser.py
@@ -20,11 +20,37 @@ class AlgebraicParser:
         if not self.is_at_end():
             return self.tokens[self.current+1]
         return None
-    def eat(self, type):
-        if self.peek().type == type:
+    def eat(self, tipo):
+        if self.peek().type == tipo:
             self.advance()
             return True
         return False
+    def prev(self):
+        return self.tokens[self.current-1]
+    def eat_more(*types):
+        if self.peek().type in types:
+            self.advance()
+            return True
+        return False
+    def parse_logic(self):
+        fnode = self.parse_comp()
+        while self.eat_more(tokens.TokenType.AND, tokens.TokenType.OR, tokens.TokenType.XOR):
+            pass
+    def parse_comp(self):
+        fnode = self.parse_expr()
+        while self.eat_more(tokens.TokenType.EQUAL_EQUAL, tokens.TokenType.BANG_EQUAL, tokens.TokenType.LESS, tokens.TokenType.GREATER, tokens.TokenType.LESS_EQUAL, tokens.TokenType.GREATER_EQUAL):
+            pass
+    def parse_expr(self):
+        fnode = self.parse_term()
+        while self.eat_more(tokens.TokenType.PLUS, tokens.TokenType.MINUS):
+            pass
+    def parse_term(self):
+        fnode = self.parse_factor()
+        while self.eat_more(tokens.TokenType.STAR, tokens.TokenType.SLASH):
+            pass
+
+    def parse_factor(self):
+        pass
 
 
 class ZynkLParser:
@@ -48,6 +74,13 @@ class ZynkLParser:
         return None
     def eat(self, type):
         if self.peek().type == type:
+            self.advance()
+            return True
+        return False
+    def prev(self):
+        return self.tokens[self.current-1]
+    def eat_more(*types):
+        if self.peek().type in types:
             self.advance()
             return True
         return False
